5aa6227beb111ac1f32257ff2c8ec7107539f5ac
Cysan <53166358+18445@users.noreply.github.com>
2022-03-12 15:05:04 +0800

    demo commit

---
+++ Demo.kt
@@ -0,0 +1,21 @@
+/**
+ * @createTime 2022年03月12日 15:03:00
+ * @ClassName Demo
+ * @version 1.0.0
+ * @author Yan
+ * @Description TODO
+ */
+class Demo {
+
+    fun doSomething(){
+
+    }
+
+}
+
+
+
+
+
+
+
